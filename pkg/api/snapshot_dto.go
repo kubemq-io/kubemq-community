76cfa906d9322d4c0277409637e262a11b2d601b
Lior Nabat <lior.nabat@gmail.clom>
2022-08-17 16:23:04 +0300

    add api entities

---
+++ pkg/api/snapshot_dto.go
@@ -0,0 +1,166 @@
+package api
+
+import (
+	"fmt"
+	"github.com/dustin/go-humanize"
+	"sort"
+	"time"
+)
+
+type SnapshotDTO struct {
+	HostsInfo            []*HostInfoDTO   `json:"hostsInfo"`
+	StatsCards           *StatCardDTOs    `json:"statsCards"`
+	TopChannels          []*TopChannelDTO `json:"topChannels"`
+	QueuesGroup          *GroupDTO        `json:"queuesGroup"`
+	PubsubGroup          *GroupDTO        `json:"pubsubGroup"`
+	CommandsQueriesGroup *GroupDTO        `json:"commandsQueriesGroup"`
+	LastActivity         int64            `json:"lastActivity"`
+	LastActivityHuman    string           `json:"lastActivityHuman"`
+	Total                *BaseValuesDTO   `json:"total"`
+	Incoming             *BaseValuesDTO   `json:"incoming"`
+	Outgoing             *BaseValuesDTO   `json:"outgoing"`
+	Channels             int64            `json:"channels"`
+	ChannelsHuman        string           `json:"channelsHuman"`
+	Clients              int64            `json:"clients"`
+	ClientsHuman         string           `json:"clientsHuman"`
+	ActiveChannels       int64            `json:"activeChannels"`
+	Queues               *FamilyDTO       `json:"queues"`
+	Pubsub               *FamilyDTO       `json:"pubsub"`
+	CommandsQueries      *FamilyDTO       `json:"commandsQueries"`
+	inBaseValues         *BaseValues
+	outBaseValues        *BaseValues
+}
+
+func newSnapshot(system *System) *SnapshotDTO {
+	return &SnapshotDTO{
+		HostsInfo:       []*HostInfoDTO{NewHostInfoDTO(system)},
+		StatsCards:      NewStatCardDTOs(),
+		Queues:          newFamilyDTO("queues"),
+		Pubsub:          newFamilyDTO("pubsub"),
+		CommandsQueries: newFamilyDTO("commandsQueries"),
+		inBaseValues:    NewBaseValues(),
+		outBaseValues:   NewBaseValues(),
+	}
+}
+func NewSnapshotDTO(system *System, entitiesGroup *EntitiesGroup) *SnapshotDTO {
+	group := newSnapshot(system)
+	queueFamily, ok := entitiesGroup.GetFamily("queues")
+	if ok {
+		group.Queues = NewFamilyDTO(queueFamily)
+		group.inBaseValues.Add(group.Queues.inBaseValues)
+		group.outBaseValues.Add(group.Queues.outBaseValues)
+		group.Channels += group.Queues.Channels
+		group.ActiveChannels += group.Queues.ActiveChannels
+		group.Clients += group.Queues.Clients
+		if group.LastActivity < group.Queues.LastActivity {
+			group.LastActivity = group.Queues.LastActivity
+		}
+	}
+
+	eventsFamily, ok := entitiesGroup.GetFamily("events")
+	if ok {
+		group.Pubsub = NewFamilyDTO(eventsFamily)
+
+	}
+	eventsStoreFamily, ok := entitiesGroup.GetFamily("events_store")
+	if ok {
+		group.Pubsub.Add(NewFamilyDTO(eventsStoreFamily))
+	}
+	group.inBaseValues.Add(group.Pubsub.inBaseValues)
+	group.outBaseValues.Add(group.Pubsub.outBaseValues)
+	group.Clients += group.Pubsub.Clients
+	group.ActiveChannels += group.Pubsub.ActiveChannels
+	group.Channels += int64(len(group.Pubsub.ChannelsList))
+	if group.LastActivity < group.Pubsub.LastActivity {
+		group.LastActivity = group.Pubsub.LastActivity
+	}
+	group.Pubsub.Name = "pubsub"
+
+	commandsFamily, ok := entitiesGroup.GetFamily("commands")
+	if ok {
+		group.CommandsQueries = NewFamilyDTO(commandsFamily)
+	}
+
+	queriesFamily, ok := entitiesGroup.GetFamily("queries")
+	if ok {
+		group.CommandsQueries.Add(NewFamilyDTO(queriesFamily))
+
+	}
+	group.inBaseValues.Add(group.CommandsQueries.inBaseValues)
+	group.outBaseValues.Add(group.CommandsQueries.outBaseValues)
+	group.Clients += group.CommandsQueries.Clients
+	group.ActiveChannels += group.CommandsQueries.ActiveChannels
+	group.Channels += int64(len(group.CommandsQueries.ChannelsList))
+	if group.LastActivity < group.CommandsQueries.LastActivity {
+		group.LastActivity = group.CommandsQueries.LastActivity
+	}
+	group.Channels += int64(len(group.CommandsQueries.ChannelsList))
+	group.CommandsQueries.Name = "request_reply"
+
+	group.Incoming = NewBaseValuesDTOFromBaseValues(group.inBaseValues)
+	group.Outgoing = NewBaseValuesDTOFromBaseValues(group.outBaseValues)
+	group.Total = NewBaseValuesDTOFromBaseValues(group.inBaseValues.CombineWIth(group.outBaseValues))
+	group.LastActivityHuman = humanize.Time(time.UnixMilli(group.LastActivity))
+	group.ChannelsHuman = humanize.Comma(group.Channels)
+	group.ClientsHuman = humanize.Comma(group.Clients)
+	group.UpdateStatCards()
+	group.CreateTopChannels()
+	group.UpdateGroupsInfo()
+	return group
+}
+
+func (g *SnapshotDTO) UpdateStatCards() *SnapshotDTO {
+	g.StatsCards.AddChannels(
+		fmt.Sprintf("%d", g.ActiveChannels),
+		fmt.Sprintf("%d", g.Channels),
+		g.Queues,
+		g.Pubsub,
+		g.CommandsQueries,
+	)
+	g.StatsCards.AddClients(
+		fmt.Sprintf("%d", g.Clients),
+		g.Queues,
+		g.Pubsub,
+		g.CommandsQueries,
+	)
+	g.StatsCards.AddIncoming(
+		g.Incoming.MessagesHumanized,
+		g.Incoming.VolumeHumanized,
+		g.Queues,
+		g.Pubsub,
+		g.CommandsQueries)
+
+	g.StatsCards.AddOutgoing(
+		g.Outgoing.MessagesHumanized,
+		g.Outgoing.VolumeHumanized,
+		g.Queues,
+		g.Pubsub,
+		g.CommandsQueries)
+	return g
+}
+
+func (g *SnapshotDTO) CreateTopChannels() *SnapshotDTO {
+	g.TopChannels = make([]*TopChannelDTO, 0)
+	var channelList []*ChannelDTO
+	channelList = append(channelList, g.Queues.ChannelsList...)
+	channelList = append(channelList, g.Pubsub.ChannelsList...)
+	channelList = append(channelList, g.CommandsQueries.ChannelsList...)
+	sort.Slice(channelList, func(i, j int) bool {
+		return channelList[i].LastActivity > channelList[j].LastActivity
+	})
+	if len(channelList) > 10 {
+		channelList = channelList[:10]
+	}
+	for _, channel := range channelList {
+		g.TopChannels = append(g.TopChannels, NewTopChannelDTO(channel))
+	}
+	return g
+}
+
+func (g *SnapshotDTO) UpdateGroupsInfo() *SnapshotDTO {
+	g.QueuesGroup = NewGroupDTO(g.Queues)
+	g.PubsubGroup = NewGroupDTO(g.Pubsub)
+	g.CommandsQueriesGroup = NewGroupDTO(g.CommandsQueries)
+	return g
+}
+
---
+++ pkg/api/top_channels_dto.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"fmt"
+	"github.com/dustin/go-humanize"
+)
+
+type TopChannelDTO struct {
+	Type         string `json:"type"`
+	Channel      string `json:"channel"`
+	LastActivity string `json:"lastActivity"`
+	Sent         string `json:"sent"`
+	Delivered    string `json:"delivered"`
+	Clients      string `json:"clients"`
+}
+
+func NewTopChannelDTO(channel *ChannelDTO) *TopChannelDTO {
+	return &TopChannelDTO{
+		Type:         transformType(channel.Type),
+		Channel:      channel.Name,
+		LastActivity: channel.LastActivityHuman,
+		Sent:         fmt.Sprintf("%s/%s", channel.Incoming.MessagesHumanized, channel.Incoming.VolumeHumanized),
+		Delivered:    fmt.Sprintf("%s/%s", channel.Outgoing.MessagesHumanized, channel.Outgoing.VolumeHumanized),
+		Clients:      humanize.Comma(channel.Incoming.Clients + channel.Outgoing.Clients),
+	}
+}
+
+func transformType(value string) string {
+	switch value {
+	case "queues":
+		return "Queue"
+	case "events":
+		return "PubSub / Events"
+	case "events_store":
+		return "PubSub / Events-Store"
+	case "commands":
+		return "Commands"
+	case "queries":
+		return "Queries"
+	default:
+		return "Unknown"
+	}
+}
